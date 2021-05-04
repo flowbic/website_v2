31d8058ba682cabb3fd7cfcad6ee4770b22f3fb3
Carl Einarsson <carl@flowbic.se>
2021-05-04 13:58:40 +0200

    adding more krimskrams at here section

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -124,7 +124,7 @@ DROPDOWN-CODE
 
 export default function HeroSection() {
   const [heroImage, setHeroImage] = useState();
-  
+
   useEffect(() => {
     const imageLoader = new Image();
     imageLoader.src = heroImageSrc;
@@ -132,7 +132,7 @@ export default function HeroSection() {
     imageLoader.onload = () => {
       setHeroImage(heroImageSrc);
     };
-  }, [])
+  }, []);
 
   return (
     <div className="bg-white">
@@ -229,6 +229,7 @@ export default function HeroSection() {
                           </Popover.Button>
                         </div>
                       </div>
+
                       <div className="mt-6">
                         <div className="mt-3 sm:mt-0 sm:ml-3">
                           {/* <a
@@ -259,6 +260,7 @@ export default function HeroSection() {
                         </nav> */}
                       </div>
                     </div>
+
                     <div className="py-6 px-5">
                       {/* {navigation.map((item) => (
                           <a
@@ -295,6 +297,77 @@ export default function HeroSection() {
         {/* Hero card */}
         <div className="relative">
           <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray" />
+          <div
+            className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full py-5"
+            aria-hidden="true"
+          >
+            <div className="relative h-full max-w-7xl mx-auto">
+              <svg
+                className="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
+                width={404}
+                height={484}
+                fill="none"
+                viewBox="0 0 404 784"
+              >
+                <defs>
+                  <pattern
+                    id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={784}
+                  fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+                />
+              </svg>
+              <svg
+                className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+                width={404}
+                height={784}
+                fill="none"
+                viewBox="0 -480 404 1084"
+              >
+                <defs>
+                  <pattern
+                    id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={784}
+                  fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
+                />
+              </svg>
+            </div>
+          </div>
           <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
               <div className="absolute inset-0">
@@ -317,7 +390,9 @@ export default function HeroSection() {
                   </span>
                 </h1>
               </div>
-          <span className="absolute text-white bottom-2 right-4 text-xl">Kalmar | Stockhom</span>
+              <span className="absolute text-white bottom-2 right-4 text-xl">
+                Kalmar | Stockhom
+              </span>
             </div>
           </div>
         </div>
